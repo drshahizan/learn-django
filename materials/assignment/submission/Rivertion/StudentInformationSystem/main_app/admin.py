a5d22ae76b4a7b39962cb06c209d92d6d142c4c7
DinieHazim <dinieh77@gmail.com>
2023-05-12 00:54:56 +0800

    Upload file

---
+++ materials/assignment/submission/Rivertion/StudentInformationSystem/main_app/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+# Register your models here.
+
+
+class UserModel(UserAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserModel)
+admin.site.register(Staff)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Subject)
+admin.site.register(Session)
