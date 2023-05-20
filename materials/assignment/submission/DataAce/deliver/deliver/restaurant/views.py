2534e7c1dd1f589afcd879d86aa5cba34e1c4eac
raihanarahim <amirah.raihanah@graduate.utm.my>
2023-05-20 10:24:38 +0800

    Update completed system

---
+++ materials/assignment/submission/DataAce/deliver/deliver/restaurant/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.utils.timezone import datetime
+from customer.models import OrderModel
+
+
+class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, *args, **kwargs):
+        # get the current date
+        today = datetime.today()
+        orders = OrderModel.objects.filter(
+            created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
+
+        # loop through the orders and add the price value, check if order is not shipped
+        unshipped_orders = []
+        total_revenue = 0
+        for order in orders:
+            total_revenue += order.price
+
+            if not order.is_shipped:
+                unshipped_orders.append(order)
+
+        # pass total number of orders and total revenue into template
+        context = {
+            'orders': unshipped_orders,
+            'total_revenue': total_revenue,
+            'total_orders': len(orders)
+        }
+
+        return render(request, 'restaurant/dashboard.html', context)
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
+    
+class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        context = {
+            'order': order
+        }
+        
+        return render(request, 'restaurant/order-details.html', context)
+    
+    def post(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        order.is_shipped = True
+        order.save()
+
+        context = {
+            'order': order
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
+    
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
--- materials/assignment/submission/DataAce/deliver/djangoenv/Scripts/activate.bat
+++ materials/assignment/submission/DataAce/deliver/djangoenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\raiha\deliver\djangoenv
+set VIRTUAL_ENV=C:\Users\user\OneDrive\Documents\DataEng\SECP3843 - STDE\deliver\djangoenv
 
 if not defined PROMPT set PROMPT=$P$G
 
